c9b9bb0ebad6e0000f074736b603b001b5f21780
Carlos Molina <carlosm12.inves@gmail.com>
2024-09-25 17:51:08 +0000

    Home en preceso

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,19 +1,24 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 
+import imagen_Home from "../../img/imagen_Home.jpg";
+
 import "../../styles/home.css";
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<div className="body">
-			<div className="descriptionHome">
+		<div className="bodyHome">
+			<div className="descripcionHome">
 				<h1>All the food for the week in one place</h1>
 				<br></br>
 				<h4>Chef at Home transforms the way to explore, plan, and organize gourmet recipes.</h4>
 				<br></br>
 				<h4>Save your favorite recipes, add personalized notes, and organize your weekly menu.</h4>
+			</div>
+			<div>
+				<img className="imagenHome" src={imagen_Home} alt="Imagen de una chica buscando recetas"/>
 			</div>
 		</div>
 	);
