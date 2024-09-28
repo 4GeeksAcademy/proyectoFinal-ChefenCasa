5f12aa8b66a8c2e269c50fc1071ba293e02461f5
Natalia Iacono <167995391+NataliaIacono@users.noreply.github.com>
2024-09-28 06:31:55 +0000

    card diseño ok

--- src/front/js/component/cardPrivada.js
+++ src/front/js/component/cardPrivada.js
@@ -5,11 +5,25 @@ import "../../styles/cardPrivada.css";
 export const CardPrivada = () => {
     return (
         <div className="contenedor-card">
-            <img src="image-url.jpg" alt="Recipe Image" class="recipe-image"/>
+            <img src="image-url.jpg" alt="Recipe Image" className="recipe-image" />
+            <div className="toda info">
                 <div className="card-content">
                     <h4 className="title">Recipe Title</h4>
-                    <p className="time"><i className="fa-regular fa-clock"></i> 45 minutes</p>
                 </div>
+
+                <div className="datos d-flex">
+
+                    <div className="reloj me-5">
+                        <p className="time"><i className="fa-regular fa-clock"></i> 45 minutes</p>
+                    </div>
+
+                    <div className="corazon-info d-flex">
+                        <p><i className="fa-solid fa-heart me-2"></i></p>
+                        <button className="botonMasInfo">info</button>
+                    </div>
+
+                </div>
+            </div>
         </div>
 
     );
