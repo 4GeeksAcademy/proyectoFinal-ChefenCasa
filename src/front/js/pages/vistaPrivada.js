a8283d80c5f12e6c4b7df3e85ed5b54619871858
Carlos Molina <carlosm12.inves@gmail.com>
2024-09-30 08:13:32 +0000

    vista privada OK

--- src/front/js/pages/vistaPrivada.js
+++ src/front/js/pages/vistaPrivada.js
@@ -4,21 +4,23 @@ import { Link } from "react-router-dom";
 import { NavbarPrivado } from "../component/Navbar/navbarPrivado";
 import { CardPrivada } from "../component/cardPrivada";
 
-import "../../styles/home.css";
+import "../../styles/vistaPrivada.css";
+import "../../styles/cardPrivada.css";
+
 
 export const VistaPrivada = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
         <div>
-            < NavbarPrivado />
-            <div className="contenedorCartasRecetas">
-                < CardPrivada />
-                < CardPrivada />
-                < CardPrivada />
-                < CardPrivada />
-            </div>
+        <NavbarPrivado />
+        <div className="contenedorCartasRecetas col-3">
+            <CardPrivada />
+            <CardPrivada />
+            <CardPrivada />
+            <CardPrivada />
         </div>
+    </div>
         );
     };
         
