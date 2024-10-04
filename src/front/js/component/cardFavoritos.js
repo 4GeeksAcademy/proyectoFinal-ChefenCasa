0f8de57e06d7b26a1d0a051a4fe489260dd7043d
Carlos Molina <carlosm12.inves@gmail.com>
2024-10-04 10:28:40 +0000

    a

--- src/front/js/component/cardFavoritos.js
+++ src/front/js/component/cardFavoritos.js
@@ -16,23 +16,25 @@ export const CardFavoritos = ({ receta }) => {
                     <h4 className='tituloRecetaFavorito'>{receta.title}</h4>
                     <img src={receta.image} alt={receta.title} className="imagenRecetaFavorito" />
                 </div>    
-                <div className='descripcionFavoritos'>
-                    <p>Ingredientes</p>
-                    <p className="tiempoCoccionFavorito" style={{ fontSize: "medium" }}>
-                        <i className="fa-regular fa-clock"></i> {receta.tiempo_de_coccion}
-                    </p>
-                </div>
-            </div>            
-            <div className="botonesFavoritos">
-                <div className="corazonInfoFavorito ">
-                    <button className='corazonFavorito'>
-                        <i className="fa-solid fa-heart me-2" style={{ fontSize: "large" }} onClick={handleFavoritos}></i>
-                    </button>
-                    <Link to={`/recetaCompletaPrivada/${receta.id}`}>
-                        <button className="botonMasInfoFavorito">info</button>
-                    </Link>
-                </div>
-            </div>
+                <div className="descripcionBotones">
+                    <div className='descripcionFavoritos'>
+                        <p>Ingredientes</p>
+                        <p className="tiempoCoccionFavorito" style={{ fontSize: "medium" }}>
+                            <i className="fa-regular fa-clock"></i> {receta.tiempo_de_coccion}
+                        </p>
+                    </div>
+                    <div className="botonesFavoritos">
+                        <div className="corazonInfoFavorito ">
+                            <button className='corazonFavorito'>
+                                <i className="fa-solid fa-heart me-2" style={{ fontSize: "large" }} onClick={handleFavoritos}></i>
+                            </button>
+                            <Link to={`/recetaCompletaPrivada/${receta.id}`}>
+                                <button className="botonMasInfoFavorito">info</button>
+                            </Link>
+                        </div>
+                    </div>
+                </div>    
+            </div>                
         </div>
     );
 };
