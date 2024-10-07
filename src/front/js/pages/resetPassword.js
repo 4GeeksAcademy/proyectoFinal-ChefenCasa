82461bbc495b3475e51ef2d89d2cd8a3af593865
Carlos Molina <carlosm12.inves@gmail.com>
2024-10-07 16:17:55 +0000

    vista Password sin funcionalidad

--- src/front/js/pages/resetPassword.js
+++ src/front/js/pages/resetPassword.js
@@ -1,25 +1,23 @@
-import React, { useContext, useState, useEffect } from "react";
+import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import { Navbar } from "../component/Navbar/navbar";
-import { useNavigate } from "react-router-dom";
-
-import "../../styles/resetPassword.css"
+import "../../styles/resetPassword.css";
 
 export const ResetPassword = () => {
     const { store, actions } = useContext(Context);
 
     return (
         <div>
-            < Navbar />
+            <Navbar />
             <div className="vistaResetPassword">
                 <div className="resetPasswordContenedor">
+                    <h2 className="titlePassword">Reset your password</h2>
                     <form className="inputResetPassword">
-                        <p style={{ fontSize: "large" }}> New Password: </p>
-                        <input className="inputPassword" type="password" name="password" id="password"/>
-                    </form>
-                    <form className="inputResetPassword">
-                        <p style={{ fontSize: "large" }}> Repeat Password: </p>
-                        <input className="inputPassword" type="password" name="password" id="password"/>
+                        <p style={{ fontSize: "large" }}> New password </p>
+                        <input type="password" name="password" id="password1"/>
+                        <p style={{ fontSize: "large" }}> Repeat password </p>
+                        <input type="password" name="password" id="password2"/>
+                        <button type="submit" className="submitButton">Reset</button>
                     </form>
                 </div>
             </div>
