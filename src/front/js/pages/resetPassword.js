bbbe78f9416c32b585ad12d21d667677ccade6cd
Natalia Iacono <167995391+NataliaIacono@users.noreply.github.com>
2024-10-08 17:10:58 +0000

    Funcionalidad a la vista Restablecer contraseña

--- src/front/js/pages/resetPassword.js
+++ src/front/js/pages/resetPassword.js
@@ -1,10 +1,22 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Context } from "../store/appContext";
 import { Navbar } from "../component/Navbar/navbar";
 import "../../styles/resetPassword.css";
 
 export const ResetPassword = () => {
     const { store, actions } = useContext(Context);
+    const [newPassword, setNewPassword] = useState("");
+    const [repeatPassword, setRepeatPassword] = useState("");
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        if (newPassword === repeatPassword) {
+            const token = new URLSearchParams(window.location.search).get("token"); 
+            await actions.resetPassword(token, newPassword);
+        } else {
+            alert("Las contraseñas no coinciden.");
+        }
+    };
 
     return (
         <div>
@@ -12,11 +24,11 @@ export const ResetPassword = () => {
             <div className="vistaResetPassword">
                 <div className="resetPasswordContenedor">
                     <h2 className="titlePassword">Reset your password</h2>
-                    <form className="inputResetPassword">
+                    <form className="inputResetPassword"onSubmit={handleSubmit}>
                         <p style={{ fontSize: "large" }}> New password </p>
-                        <input type="password" name="password" id="password1"/>
+                        <input type="password" name="password" id="password1" value={newPassword} onChange={(e) => setNewPassword(e.target.value)}/>
                         <p style={{ fontSize: "large" }}> Repeat password </p>
-                        <input type="password" name="password" id="password2"/>
+                        <input type="password" name="password" id="password2" value={repeatPassword} onChange={(e) => setRepeatPassword(e.target.value)}/>
                         <button type="submit" className="submitButton">Reset</button>
                     </form>
                 </div>
