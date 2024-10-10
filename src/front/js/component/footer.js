afa7e0eb931f5f30283f4d14c8cec2aef78ea7bb
Carlos Molina <carlosm12.inves@gmail.com>
2024-10-10 08:30:54 +0000

    Footer Arreglado

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -10,11 +10,6 @@ export const Footer = () => {
                 <img className="logoFooter" src={Logo} alt="Logo Chef at Home" />
             </div>
             <div className="contenedorCafe">
-                <p className="textoCafe">
-                    <i className="fas fa-heart"></i> &nbsp;
-                    If you want to help us, <br /> you can invite us to a coffee here &nbsp;
-                    <i className="fas fa-heart"></i>
-                </p>
                 <a href='https://cafecito.app/chefathome' rel='noopener' target='_blank' className="cafecito">
                     <img
                         srcSet='https://cdn.cafecito.app/imgs/buttons/button_1.png 1x, https://cdn.cafecito.app/imgs/buttons/button_1_2x.png 2x, https://cdn.cafecito.app/imgs/buttons/button_1_3.75x.png 3.75x'
