f749773f358119f6b2d334714c73e70e421b949f
Carlos Molina <carlosm12.inves@gmail.com>
2024-10-11 10:29:27 +0000

    Vista Favoritos y Vista Privada Responsive

--- src/front/js/component/cardFavoritos.js
+++ src/front/js/component/cardFavoritos.js
@@ -1,76 +1,3 @@
-// import React, { useContext, useEffect, useState } from 'react';
-// import { Link } from 'react-router-dom';
-// import { Context } from '../store/appContext';
-// import { NotaModal } from './notaModal';
-
-// export const CardFavoritos = ({ receta }) => {
-//     console.log("Receta en CardFavoritos:", receta);
-//     const { actions, store } = useContext(Context)
-//     const [show, setShowModal] = useState(false)
-
-//     const handleModal = () => {
-//         setShowModal(true)
-
-//     }
-
-//     const handleClose = () => {
-//         setShowModal(false)
-
-//     }
-
-//     // Manejo de la eliminación del favorito
-//     const handleEliminar = () => {
-
-//         actions.eliminarFav(receta.api_receta_id);
-
-
-//     };
-
-
-//     return (
-//         <div className="contenedorCardFavorito">
-//             <div className="datosCompletosFavoritos">
-//                 <div className="datosTituloImagen">
-//                     <h4 className='tituloRecetaFavorito'>{receta.receta_title}</h4>
-//                     <img src={receta.imagen} alt={receta.title} className="imagenRecetaFavorito" />
-//                     <div className="botonesFavoritos">
-//                         <div className="corazonInfoFavorito ">
-//                             <button className="botonNotaFavoritos" onClick={handleModal}>Note</button>
-//                             <button className='corazonFavorito'>
-//                                 <i className="fa-solid fa-heart me-2" style={{ fontSize: "large" }} onClick={() => handleEliminar(receta.api_receta_id)}></i>
-//                             </button>
-//                             <Link to={`/recetaCompletaPrivada/${receta.api_receta_id}`}>
-//                                 <button className="botonMasInfoFavorito">info</button>
-//                             </Link>
-//                         </div>
-//                     </div>
-//                 </div>
-
-//                 <div className="descripcionBotones">
-//                     <div className='descripcionFavoritos'>
-//                         <p className="ingredientesFavoritos">
-//                             <strong>Ingredientes:</strong>
-//                         </p>
-//                         <ul>
-//                             {receta.ingredientes && receta.ingredientes.length > 0
-//                                 ? receta.ingredientes.map((ingrediente, index) => (
-//                                     <li key={index}>{ingrediente}</li>
-//                                 ))
-//                                 : <li>Ingredientes no disponibles</li>}
-//                         </ul>
-
-//                         <p className="tiempoCoccionFavorito" style={{ fontSize: "medium" }}>
-//                             <i className="fa-regular fa-clock"></i> {receta.tiempo_de_coccion
-//                             } minutes
-//                         </p>
-//                     </div>
-//                 </div>
-//             </div>
-//             <NotaModal show={show} apiRecetaId={receta.api_receta_id} onClose={handleClose} />
-//         </div>
-//     );
-// };
-
 import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Context } from '../store/appContext';
@@ -108,7 +35,7 @@ export const CardFavoritos = ({ receta }) => {
                             <button className='corazonFavorito'>
                                 <i 
                                     className="fa-solid fa-heart me-2" 
-                                    style={{ fontSize: "large", color: isFavorito ? "orange" : "gray" }} 
+                                    style={{ color: isFavorito ? "orange" : "gray" }} 
                                     onClick={handleEliminar}
                                 ></i>
                             </button>
@@ -124,7 +51,7 @@ export const CardFavoritos = ({ receta }) => {
                         <p className="ingredientesFavoritos">
                             <strong>Ingredientes:</strong>
                         </p>
-                        <ul>
+                        <ul className='ingredientesFavoritos'>
                             {receta.ingredientes && receta.ingredientes.length > 0
                                 ? receta.ingredientes.map((ingrediente, index) => (
                                     <li key={index}>{ingrediente}</li>
@@ -132,7 +59,7 @@ export const CardFavoritos = ({ receta }) => {
                                 : <li>Ingredientes no disponibles</li>}
                         </ul>
 
-                        <p className="tiempoCoccionFavorito" style={{ fontSize: "medium" }}>
+                        <p className="tiempoCoccionFavorito">
                             <i className="fa-regular fa-clock"></i> {receta.tiempo_de_coccion} minutes
                         </p>
                     </div>
