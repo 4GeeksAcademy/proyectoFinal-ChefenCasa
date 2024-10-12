105fa0c88eaa2fe6283c1310ace7e4dbd3d37746
aaguirre <alejandra36a@gmail.com>
2024-10-12 09:45:11 +0000

    chatfinal con variable

--- src/front/js/component/chatBot.js
+++ src/front/js/component/chatBot.js
@@ -65,11 +65,12 @@ export const ChatBot = () => {
             // Comprobamos si el input es válido para redirigir
             if (input !== '1' && input !== '2' && input !== '3') {
                 // La dirección a la que haremos los pedidos
+                const apiKeyOpenChat = process.env.REACT_APP_OPENIA_API_KEY
                 const response = await fetch('https://api.openai.com/v1/chat/completions', {
                     method: 'POST',
                     headers: {
                         "Content-Type": 'application/json',
-                        "Authorization": 'Bearer process.env.REACT_APP_OPENIA_API_KEY '
+                        "Authorization": 'Bearer ' + apiKeyOpenChat
                     },
                     body: JSON.stringify({
                         model: 'gpt-3.5-turbo', // Módulo gratuito
